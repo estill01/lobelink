b77c1ff121bb298938db0a01d2cd6ecf2a13f1cc
estill01 <ehs.stillman@gmail.com>
2011-09-02 16:41:12 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+gem 'sqlite3'
+gem 'nifty-generators'
+
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+# Deploy with Capistrano
+# gem 'capistrano'
+
+group :test do
+  gem 'turn', :require => false
+end
+gem "bcrypt-ruby", :require => "bcrypt"
+gem "mocha", :group => :test
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frontdoor::Application.config.secret_token = 'e14b526ae690e2c71a5c4e7505acb7bd6049aeb294a38ab48446b5bd99e7e214feabb86a1f856193647fca6172f57c24f3d6b42c216e9fcc0af4dcaf069c3037'

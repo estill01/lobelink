27675bf7d4e26a3533045fd93065b88a801c7814
estill01 <ehs.stillman@gmail.com>
2011-09-03 04:18:23 -0500

    heroku fixes

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,11 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require *Rails.groups(:assets => %w(development test))
+
+#Heroku config requirement
+	Bundler.require *Rails.groups(:assets) if defined?(Bundler) 
+
+	#Bundler.require *Rails.groups(:assets => %w(development test))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end

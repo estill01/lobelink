968489bdd2df42cfda537d84bb00bd22f26616ef
estill01 <ehs.stillman@gmail.com>
2011-09-05 06:29:21 -0500

    nav css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include ControllerAuthentication
   protect_from_forgery
-	check_authorization
+#	check_authorization 
 	rescue_from CanCan::AccessDenied do |exception|
 		flash[:error] = "Access denied. Looks like you're not allowed to do that." 
 		redirect_to root_path
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
-	skip_authorization_check
+#	authorize_resource
+ skip_authorization_check
 
   def new
   end

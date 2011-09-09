affb3170841f8ad59e45d8873412b25c18db5d96
estill01 <ehs.stillman@gmail.com>
2011-09-09 13:15:23 -0500

    newest policies listing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
 		featured = Policy.find(:first, :order => "created_at DESC")
 		return User.find(featured.user_id)
 	end
+
+	def newest_policies
+		newest = Policy.find(:all, :order => "created_at DESC", :limit => 2)
+		@newest = []
+		newest.each do |policy|
+			@newest << User.find(policy.user_id)
+		end
+		return @newest
+	end
 end
